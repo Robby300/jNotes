c6154e4d02ab4c2d992e2ee9138197a57c4b4e3d
Robert Imangulov <robby300im@gmail.com>
2022-02-19 17:30:29 +0500

    add Dockerfile

--- src/main/java/com/project/jNotes/repo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.jNotes.repo;
-
-import com.project.jNotes.domens.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-    User findByUsername(String username);
-}
