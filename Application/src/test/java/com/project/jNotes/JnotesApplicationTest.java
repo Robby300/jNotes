4c997c0821458936cbca53a88915e8ec16da0a1b
robert <robby300im@gmail.com>
2022-02-21 11:06:33 +0500

    add main test

---
+++ Application/src/test/java/com/project/jNotes/JnotesApplicationTest.java
@@ -0,0 +1,12 @@
+package com.project.jNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = JnotesApplicationTest.class)
+class JnotesApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
